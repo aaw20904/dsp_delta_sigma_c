2fddf23f1e2991689828d4c1d02678a58c35a214
Andrii Androsovych <55437123+aaw20904@users.noreply.github.com>
2024-11-13 22:15:16 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -2,44 +2,72 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+unsigned int streamIIRFilter(unsigned int bitOfStream);
 
 int main(int argc, char *argv[]) {
-	printf("%d",sizeof(int));
+	
+	streamIIRFilter (0x00000001);
+	for (int var=0; var<128; var++) {
+		 streamIIRFilter (0x00000000);
+	}
 	return 0;
 }
 //converts bit-stream into 32-bit integer chunk of data
 //This function save previous results in an static array
-unsigned int streamIIRFilter(unsigned char* bitStream,  unsigned int * intData){
-	static unsigned int filterWindow 
+/* 128 Tap Low Pass Hamming                                                   
+  Finite Impulse Response                                                     
+  Sample Frequency = 384.0 KHz                                                
+  Standard Form                                                               
+  Arithmetic Precision = 4 Digits                                                                                                                          
+  Pass Band Frequency = 3.000 KHz  */
+  //sum all the coefs equals 65534
+unsigned int streamIIRFilter(unsigned int bitOfStream){
+
+	static unsigned int filterWindow [129]={0};
+	static unsigned int bits [16]={0};
+	
+	const unsigned int coefs[]={1,2,4,6,8,11,14,17,21,26,31,37,44,52,61,71,82,94,108,122,
+		138,156,174,194,216,238,262,287,313,341,369,399,430,461,493,526,560,594,628,663,697,
+		732,767,801,834,868,900,931,962,991,1019,1046,1071,1094,1116,1136,1153,1169,1183,
+		1195,1204,1210,1216,1218,1218,1216,1210,1204,1195,1183,1169,1153,1136,1116,1094,
+		1071,1046,1019,991,962,931,900,868,834,801,767,732,697,663,628,594,560,526,493,
+		461,430,399,369,341,313,287,262,238,216,194,174,156,138,122,108,94,82,71,61,52,
+		44,37,31,26,21,17,14,11,8,6,4,2,1 };
+		bitOfStream &= 0x00000001;
+		
+		//1)shift word 3
+		bits[3] <<= 1;
+		
+		//1)has the word 2 MSB bit?
+		if (bits[2] & 0x80000000) {
+			bits[3] |= 1; //apply bit to higher word
+			bits[2] <<= 1; //shift current 
+		
+		}else {
+			bits[2] <<= 1;
+		}
+	
+		//3)Has word 1 MSB?
+		if (bits[1] & 0x80000000) {
+		    bits[2] |= 1; //apply to higher
+			bits[1] <<= 1; //shift current
+			
+		} else {
+			bits[1] <<= 1;
+		}
+			//3)Has word 0 MSB?
+		if (bits[0] & 0x80000000) {
+			bits[0] <<= 1;
+			bits[1] |= 1;
+		} else {
+			bits[0] <<= 1;
+		}
+			//2)apply input bit of stream to word 0
+		
+		bits[0] |= bitOfStream;
+		
+		
+		printf("%X, %X, %X, %X \n",bits[0], bits[1], bits[2], bits[3]);
+		
 }
 
-const fs = require('fs');
-//blackman 4 dig prec
-let coefs= [  0.0,1.159e-07,7.864e-07,2.523e-06,5.88e-06,1.146e-05,1.993e-05,3.202e-05,4.852e-05,7.03e-05,
-        9.831e-05,1.336e-04,1.773e-04,2.305e-04,2.946e-04,3.709e-04,4.609e-04,5.659e-04,6.874e-04,8.271e-04,
-        9.865e-04,1.167e-03,1.37e-03,1.598e-03,1.85e-03,2.13e-03,2.438e-03,2.774e-03,3.14e-03,3.536e-03,
-        3.963e-03,4.421e-03,4.909e-03,5.427e-03,5.975e-03,6.551e-03,7.154e-03,7.783e-03,8.434e-03,9.107e-03,
-        9.797e-03,1.05e-02,1.122e-02,1.195e-02,1.268e-02,1.341e-02,1.414e-02,1.486e-02,1.556e-02,1.625e-02,
-        1.692e-02,1.757e-02,1.818e-02,1.876e-02,1.93e-02,1.98e-02,2.026e-02,2.066e-02,2.102e-02,2.132e-02,
-        2.156e-02,2.174e-02,2.186e-02,2.192e-02,2.192e-02,2.186e-02,2.174e-02,2.156e-02,2.132e-02,2.102e-02,
-        2.066e-02,2.026e-02,1.98e-02,1.93e-02,1.876e-02,1.818e-02,1.757e-02,1.692e-02,1.625e-02,1.556e-02,
-        1.486e-02,1.414e-02,1.341e-02,1.268e-02,1.195e-02,1.122e-02,1.05e-02,9.797e-03,9.107e-03,8.434e-03,
-        7.783e-03,7.154e-03,6.551e-03,5.975e-03,5.427e-03,4.909e-03,4.421e-03,3.963e-03,3.536e-03,3.14e-03,
-        2.774e-03,2.438e-03,2.13e-03,1.85e-03,1.598e-03,1.37e-03,1.167e-03,9.865e-04,8.271e-04,6.874e-04,
-        5.659e-04,4.609e-04,3.709e-04,2.946e-04,2.305e-04,1.773e-04,1.336e-04,9.831e-05,7.03e-05,4.852e-05,
-        3.202e-05,1.993e-05,1.146e-05,5.88e-06,2.523e-06,7.864e-07,1.159e-07,0.0];
-      coefs =   coefs.map((val,idx)=>Math.round(val*65535)); // 524280 >> 3 = 65535
-let another = Array.from(coefs)
-
-for (let q=coefs.length-1; q>=0; q--){
-    another.push(coefs[q]);
-}
-let content='['
-let sum=0;
-another.forEach((v,idx)=>{
-    content +=`${v},`
-     sum += v
-    })
-    console.log(sum)
-content += '], shitf right on 3 bits to have 65535 in sum';
-fs.writeFileSync('./Koefs.txt',content);
